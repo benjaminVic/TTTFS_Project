7e88928186268ec718079f64ace1c0b55ec25fa1
CactusTribe <lefrancjoaquim@gmail.com>
2015-12-01 16:49:39 +0100

    Premiere approche pour ll, gestion d'une table de disques, start_disk()

--- ll/header/ll.h
+++ ll/header/ll.h
@@ -1,30 +1,33 @@
 #define MAX_OPEN_DISK 64
 #define BLCK_SIZE 1024 // 1024 Octects
-#define PATH_SATATFS "/tfs/" // Emplacement des disques TFS
+#define PATH_TFS "/tfs/" // Emplacement des disques TFS
 
+// Erreurs
 #define _NOERROR 0
-#define _ERROR 1
+#define _DISK_NOT_FOUND 1
+#define _DISK_UNMOUNTED 2
+#define _BLCK_NOT_FOUND 3
+#define _DISK_ID_TOO_BIG 4
+#define _DISK_ID_EXIST 5
+
+// Définition des types
+typedef int error;
+typedef int disk_id;
+typedef char block[BLCK_SIZE];
 
 typedef struct{
-	int errno;
-} error;
-
-typedef struct{
-	int id;
-} disk_id;
-
-typedef struct{
-	disk_id id;
 	int disk_descriptor;
-	int size;
+	int flag;
 } DISK;
 
-typedef struct{
-	char* donnees;
-} block;
-
-error start_disk(char *name, disk_id *id);
+//_________________________________________________________
+// Fonctions principales
+error start_disk(char *name, disk_id id);
 error read_block(disk_id id, block b, uint32_t num);
 error write_block(disk_id id, block b, uint32_t num);
 error sync_disk(disk_id id);
 error stop_disk(disk_id id);
+
+//_________________________________________________________
+// Fonctions auxiliaires
+char* strError(error err);
--- ll/src/ll.c
+++ ll/src/ll.c
@@ -2,42 +2,109 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "../header/ll.h"
 
+// Flags
+#define _UNMOUNTED 0
+#define _MOUNTED 1
+
+// Prototype des fonctions internes 
+error read_physical_block(disk_id id, block b, uint32_t num);
+error write_physical_block(disk_id id, block b, uint32_t num);
+
+// Emplacements disponibles pour les disques
 DISK _disks[MAX_OPEN_DISK] = {};
 
-
+//_____________________________________________________________
+// ZONE DE TEST (Les opérations ici ce feront dans la couche supérieure)
 int main(int argc, char *argv[]){
-	//****************************
-	// ZONE DE TEST
-	//****************************
 
-	disk_id disk0;
-	disk0.id = 0;
+	block block_lu;
 
-	start_disk("zDisk.tfs", disk0);
-
-
+	error start = start_disk("zDisk.tfs", 0);
+	
+	// Si le disque est bien démarré 
+	if(start == 0){
+		// On tente de lire le block x 
+		error read = read_physical_block(0, block_lu, 0);
+		// Si la lecture à réussie, on l'affiche
+		if(read == 0){
+			//printf("%s\n", block_lu);
+		}
+		else{
+			fprintf(stderr, "Erreur %d: %s\n", read, strError(read));
+		}
+	}
+	else{
+		fprintf(stderr, "Erreur %d: %s\n", start, strError(start));
+	}
 	return 0;
 }
+//_____________________________________________________________
 
+//_____________________________________________________________
+// Fonctions internes non visibles de l'extérieurs (Pas de signatures dans le .h)
 error read_physical_block(disk_id id, block b, uint32_t num){
-	error errorTest;
-	errorTest.errno = 0;
-	return errorTest;
+
+	// Si le disque est monté
+	if((_disks[id].flag & _MOUNTED) != 0){
+		// On lit les 1024 octects 
+		return _NOERROR;
+	}
+	else
+		return _DISK_UNMOUNTED;
 }
 
 error write_physical_block(disk_id id, block b, uint32_t num){
-	error errorTest;
-	errorTest.errno = 0;
-	return errorTest;
+	return _NOERROR;
 }
 
-error start_disk(char *name, disk_id *id){
-	DISK* disk = 0;
-	int disk_file = 0;
+//_____________________________________________________________
+// Fonctions externes
+error start_disk(char *name, disk_id id){
 
+	// Si l'id demandée est comprise entre 0 et MAX_OPEN_DISK-1
+	if(id < MAX_OPEN_DISK){
+		// Si l'emplacement est libre pour cette id (Le disque n'est pas monté)
+		if(_disks[id].disk_descriptor == 0){
 
-	error errorTest = {0};
-	return errorTest;	
+			int disk_file = open(name, O_RDWR);
+			// Si le disque (le fichier) existe
+			if(disk_file != -1){
+				// On le démarre en lui affectant le descripteur de fichier et le flags _MOUNTED
+				_disks[id].disk_descriptor = disk_file;
+				_disks[id].flag = _MOUNTED;
+
+				return _NOERROR;
+			}	
+			else
+				return _DISK_NOT_FOUND;
+		}
+		else
+			return _DISK_ID_EXIST;
+	}
+	else
+		return _DISK_ID_TOO_BIG;
+}
+
+error stop_disk(disk_id id){
+	return _NOERROR;
+}
+
+//_____________________________________________________________
+// Fonctions auxiliaires
+char* strError(error err){
+	if(err == _DISK_NOT_FOUND)
+		return "Le disque n'existe pas.";
+	if(err == _DISK_UNMOUNTED)
+		return "Le disque n'est pas monté.";
+	if(err == _BLCK_NOT_FOUND)
+		return "Le numéro de block est plus grand que la taille du disque.";
+	if(err == _DISK_ID_TOO_BIG)
+		return "L'id demandée doit être inférieur à MAX_OPEN_DISK.";
+	if(err == _DISK_ID_EXIST)
+		return "L'id demandée existe déjà.";
+
+	return "Aucune erreur.";
 }
